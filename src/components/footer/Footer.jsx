da8a512085019dcc55a8803d7cee6111c2a409af
Timilehin Arigbede <harriemannie@gmail.com>
2024-01-06 19:57:59 +0000

    Footer Section Design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>wolfdev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link href={"/"} className={styles.logo}>
-        Clwolf
+        WolfDev
       </Link>
       <div>
         <Links />
